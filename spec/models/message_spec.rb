9be783d86bf3fc9a9d281b68ea192fad56a610ab
bguyza <1679571+bguyza@users.noreply.github.com>
2020-05-21 22:14:08 -0600

    Added some model tests

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:chat) { create(:chat) }
+  let!(:user) { create(:user) }
+  let!(:user2) { create(:user, name: 'Val') }
+  let!(:user_chat) { create(:user_chat, user_id: user.id, chat_id: chat.id) }
+
+  it 'is valid with valid attributes' do
+    expect(Message.new(chat: chat, user: user)).to be_valid
+  end
+
+  it 'is not valid without a user' do
+    expect(Message.new(chat: chat)).to_not be_valid
+  end
+
+  it 'is not valid without a chat' do
+    expect(Message.new(user: user)).to_not be_valid
+  end
+
+  it 'is not valid if user does not belong to associated chat' do
+    expect(Message.new(chat: chat, user: user2)).to_not be_valid
+  end
 end
